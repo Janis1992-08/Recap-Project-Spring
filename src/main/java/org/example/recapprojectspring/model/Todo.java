bfd7da60f9f61c1a2928675023bc1802bb773a19
Janis Schiller <schillerjanis@gmail.com>
2024-06-24 16:17:15 +0200

    Initial Commit

--- src/main/java/org/example/recapprojectspring/model/Todo.java
+++ src/main/java/org/example/recapprojectspring/model/Todo.java
@@ -3,10 +3,8 @@ package org.example.recapprojectspring.model;
 
 import lombok.With;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @With
-//@Document(collection = "todo")
 public record Todo(
         @Id
         String id,
